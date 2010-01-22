ca7ba3391212b5e7dda3f569d0deb014f52168c8
James Marca <jmarca@translab.its.uci.edu>
2010-01-21 17:21:41 -0800

    copied the relative url stuff from the redmine textile helper.rb

--- app/helpers/redmine_markdown_formatter/helper.rb
+++ app/helpers/redmine_markdown_formatter/helper.rb
@@ -4,9 +4,10 @@ module RedmineMarkdownFormatter
 
     def wikitoolbar_for(field_id)
       file = Engines::RailsExtensions::AssetHelpers.plugin_asset_path('redmine_markdown_formatter', 'help', 'markdown_syntax.html')
+      url = "#{Redmine::Utils.relative_url_root}"+file
       help_link = l(:setting_text_formatting) + ': ' +
-      link_to(l(:label_help), file,
-              :onclick => "window.open(\"#{file}\", \"\", \"resizable=yes, location=no, width=300, height=640, menubar=no, status=no, scrollbars=yes\"); return false;")
+      link_to(l(:label_help), url,
+              :onclick => "window.open(\"#{url}\", \"\", \"resizable=yes, location=no, width=300, height=640, menubar=no, status=no, scrollbars=yes\"); return false;")
 
       javascript_include_tag('jstoolbar/jstoolbar') +
         javascript_include_tag('markdown', :plugin => 'redmine_markdown_formatter') +
